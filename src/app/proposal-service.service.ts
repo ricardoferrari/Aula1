2c7154e5fcd9bb90a02fcf4ff42d4b4aa584b077
Ricardo A. Ferrari <ricardoaugusto.ferrari@gmail.com>
2024-03-23 12:04:39 -0300

    chore(material): including design system

--- src/app/proposal-service.service.ts
+++ src/app/proposal-service.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Observable, catchError, map, of, timeout } from 'rxjs';
 
 
 export interface ProductResolved {
@@ -17,16 +17,17 @@ export class ProposalServiceService {
     const source: Observable<boolean> = new Observable<boolean>((observer) => {
       setTimeout(() => {
         observer.error('Proposal not found');
-      }, 1000);
-      // setTimeout(() => {
-      //   observer.next(true);
-      // }, 2000);
+      }, 10000);
+      setTimeout(() => {
+        observer.next(true);
+      }, 15000);
     });
 
     return source.pipe(
-      map(_proposal => {
-        return { proposal: _proposal};
+      map(response => {
+        return { proposal: response};
       }),
+      timeout(10000),
       catchError(() => of({ proposal: undefined, error: 'Proposal not found'}))
     );
   }
